38a31f3658cec3584fb25aa42f5ce7831fdec5b6
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-10 17:31:08 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,12 +5,12 @@ SG_ID="sg-009ffff77088a7662"
 
 for instance in $@
 do
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-009ffff77088a7662 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+  INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
 
   if [ $instance != "frontend"]; then
-        IP=$(aws ec2 describe-instances --instance-ids i-0116a43fdf2a27c13 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text --region us-east-1)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text --region us-east-1)
   else
-        IP=$(aws ec2 describe-instances --instance-ids i-0116a43fdf2a27c13 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region us-east-1)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region us-east-1)
   fi
 
     echo "$instance: $IP"
