d41a3627a3a879c4ef1221e8330df3c5cacfd732
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-10 18:34:07 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,15 +2,37 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-009ffff77088a7662"
-
+ZONE_ID="Z063603412VOFRSAXAFPR"
+DOMAIN_NAME="rajamouli.online"
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text --region us-east-1)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text --region us-east-1)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating the record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 done
