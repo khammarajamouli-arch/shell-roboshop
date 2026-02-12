b74b884f1bf328bbb79a32cb955fc04fa0c1ed1c
Rajamouli Khamma <khammarajamouli@gmail.com>
2026-02-13 00:08:07 +0530

    hello world

---
+++ frontend.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MONGODB_HOST=mongodb.rajamouli.online
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started at : $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privilage"
+    exit 1
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+         echo -e "$2 ... $R is failure $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo  -e "$2 .... $G is success $N" | tee -a $LOG_FILE
+    fi
+
+}
+
+
+dnf module disable nginx -y &>>$LOG_FILE
+VALIDATE $? "disabling nginx"
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "enabling nginx"
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "enabling nginx"
+
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "starting nginx"
+
+rm -rf /usr/share/nginx/html/* 
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "unzip nginx"
+
+rm -rf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "restarting nginx"
